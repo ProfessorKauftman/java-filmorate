22281aa10cf06596d78b19cc2a11dba671cf2e0e
Professor <maxkauftman@yandex.ru>
2024-04-08 00:19:00 +0300

    Feat: добавение add-feed

--- src/main/java/ru/yandex/practicum/filmorate/model/Event.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Event.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Data
 @RequiredArgsConstructor
